5b3543e98853788f509d926bb446504f9ceacf22
James Noria <jnoria.dev@gmail.com>
2024-02-26 16:40:58 -0500

    fix: minor changes on space rendering

--- init.lua
+++ init.lua
@@ -4,17 +4,3 @@ require("core.plugin_config")
 
 vim.wo.number = true
 vim.opt.showmode = false
-
--- Set listchars
-vim.o.listchars = 'nbsp:␣,eol:↲,tab:»\\ ,extends:›,precedes:‹,trail:•'
-
--- Set showbreak
-vim.o.showbreak = '↳ '
-
--- Disable number column in visual mode
-vim.api.nvim_exec([[
-  augroup my_visuallistchars
-    autocmd!
-    autocmd CursorMoved * if mode() =~# "[vV\<C-v>]" | set list | else | set nolist | endif
-  augroup END
-]], false)
